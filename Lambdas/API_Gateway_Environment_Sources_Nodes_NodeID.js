859fca2cbf4b232fddef3c8f7316910ddfd437b9
Martyn Messerli <realloser@gmx.ch>
2018-09-03 23:06:45 +0200

    Allow attribute filter and fix between filter

--- Lambdas/API_Gateway_Environment_Sources_Nodes_NodeID.js
+++ Lambdas/API_Gateway_Environment_Sources_Nodes_NodeID.js
@@ -19,9 +19,18 @@ exports.handler = (event, context, callback) => {
         callback('failed');
     }
 
-    const table_name = 'EnvironmentData';
-    const node_id = event.pathParameters['node_id'];
-    
+    const queryContext = {
+        tableName: 'EnvironmentData',
+        node: event.pathParameters['node_id'],
+        endTime: new Date().getTime() - 5 * 60 * 60 * 1000,
+    };
+
+    if ('proxy' in event.pathParameters) {
+        const proxyArray = event.pathParameters['proxy'].split('/') || [];
+
+        queryContext.top = proxyArray[0].toLowerCase() === 'top';
+    }
+
     // const done = doneConsole;
     const done = (err, res) => {
         console.timeEnd('query');
@@ -33,7 +42,7 @@ exports.handler = (event, context, callback) => {
         else {
             response = {
                 statusCode: '200',
-                body: mapResponse(node_id, res),
+                body: mapResponse(queryContext, res),
                 headers: {
                     'Content-Type': 'application/json',
                 }
@@ -46,13 +55,11 @@ exports.handler = (event, context, callback) => {
 
 
 
-    // get the last 5 hours
-    const endTime = new Date().getTime();
-    const startTime = endTime - 5 * 60 * 60 * 1000;
+
 
     switch (event.httpMethod) {
         case 'GET':
-            queryTable(table_name, node_id, startTime, endTime, done);
+            queryTable(queryContext, done);
             break;
         default:
             done(new Error(`Unsupported method "${event.httpMethod}"`));
@@ -82,32 +89,67 @@ const errorHandling = (err) => {
     }
 };
 
-const queryTable = function (tableName, node_id, startTime, endTime, callback) {
+const queryTable = function (context, callback) {
 
     // conditions [IN, NULL, BETWEEN, LT, NOT_CONTAINS, EQ, GT, NOT_NULL, NE, LE, BEGINS_WITH, GE, CONTAINS]
 
     var params = {
-        TableName: tableName,
-        //Limit: 2,
+        TableName: context.tableName,
         ScanIndexForward: false,
-        KeyConditions: [docClient.Condition('node', 'EQ', node_id)]
+        KeyConditions: [docClient.Condition('node', 'EQ', context.node)],
     };
 
-    params.KeyConditions.push(endTime 
-        ? docClient.Condition('timeStamp', 'BETWEEN', `${startTime}`, `${endTime}`)
-        : docClient.Condition('timeStamp', 'GE', `${startTime}`));
+    addAttributeFilter(context, params);
+
+    if (context.top) {
+        params.Limit = 1;
+    }
+    else {
+        params.KeyConditions.push(context.endTime
+            ? docClient.Condition('timeStamp', 'BETWEEN', `${context.endTime}`, `${context.startTime}`)
+            : docClient.Condition('timeStamp', 'GE', `${context.startTime}`));
+    }
 
     console.log('query params', JSON.stringify(params));
     console.time('query')
     docClient.query(params, callback);
 }
 
-const mapResponse = function(node, res) {
-    return JSON.stringify({
-        node: node,
-        data: res.Items.map(item => item.payload),
-        count: res.Count, 
-    });
+const addAttributeFilter = function (context, params) {
+    // specify a default attribute filter
+    let availableAttributes = [
+        // "node", // already part of the main body response
+        //"timeStamp", // keyword
+        "primary_temperature",
+        "secondary_temperature",
+        "batt",
+        // "messageIndex",
+        // "msgid",
+        "humidity",
+        "light_intensity",
+        "pressure"];
+
+    let keyWordAttr = ['timeStamp'];
+    if (keyWordAttr) {
+        params.ExpressionAttributeNames = keyWordAttr.reduce((obj, attr, i) => {
+                const key = `#attr_${attr}`;
+                obj[key] = `${attr}`;
+                availableAttributes.push(key)
+                return obj;
+            }, {});
+    }
+
+    params.ProjectionExpression = availableAttributes.map((a) => `payload.${a}`).join(', ');
+};
+
+const mapResponse = function (context, res) {
+    const data = res.Items.map(item => item.payload);
+    const response = {
+        node: context.node,
+        data: context.top ? data[0] : data,
+    };
+    !context.top && (response.count = data.length);
+    return JSON.stringify(response);
 };
 
 
@@ -199,6 +241,68 @@ const mapResponse = function(node, res) {
     },
     "body": null,
     "isBase64Encoded": false
+}
+
+*/
+
+/* Request with {proxy+}
+
+{
+  "resource": "/sources/{source_id}/nodes/{node_id}",
+  "path": "/sources/thesourceid/nodes/thenodeid",
+  "httpMethod": "GET",
+  "headers": null,
+  "queryStringParameters": {
+    "query1": "aba",
+    "query2": "baba"
+  },
+  "pathParameters": {
+    "source_id": "thesourceid",
+    "node_id": "B9FC4586"
+  },
+  "stageVariables": null,
+  "requestContext": {
+    "path": "/sources/{source_id}/nodes/{node_id}",
+    "accountId": "794552060080",
+    "resourceId": "533zqp",
+    "stage": "test-invoke-stage",
+    "requestId": "984b9077-af9f-11e8-a8b3-9314f36fb329",
+    "identity": {
+      "cognitoIdentityPoolId": null,
+      "cognitoIdentityId": null,
+      "apiKey": "test-invoke-api-key",
+      "cognitoAuthenticationType": null,
+      "userArn": "arn:aws:iam::794552060080:root",
+      "apiKeyId": "test-invoke-api-key-id",
+      "userAgent": "aws-internal/3 aws-sdk-java/1.11.347 Linux/4.9.110-0.1.ac.201.71.329.metal1.x86_64 Java_HotSpot(TM)_64-Bit_Server_VM/25.172-b31 java/1.8.0_172",
+      "accountId": "794552060080",
+      "caller": "794552060080",
+      "sourceIp": "test-invoke-source-ip",
+      "accessKey": "ASIA3R7X6JCYEDYT73SI",
+      "cognitoAuthenticationProvider": null,
+      "user": "794552060080"
+    },
+    "resourcePath": "/sources/{source_id}/nodes/{node_id}",
+    "httpMethod": "GET",
+    "extendedRequestId": "Mp6IwFgnDoEFW1w=",
+    "apiId": "zjpehz8xi5"
+  },
+  "body": null,
+  "isBase64Encoded": false
+}
+
+{
+  "resource": "/sources/{source_id}/nodes/{node_id}/{proxy+}",
+  "path": "/sources//nodes/B9FC4586/top/huu/haaa",
+  "httpMethod": "GET",
+  "headers": null,
+  "queryStringParameters": null,
+  "pathParameters": {
+    "proxy": "top/huu/haaa",
+    "source_id": "",
+    "node_id": "B9FC4586"
+  },
+...
 }
 
 */
